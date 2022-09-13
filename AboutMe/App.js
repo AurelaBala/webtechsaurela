8bc18edb0c96a1513ec806bb26a19ad2a76b0a81
AurelaBala <113447554+AurelaBala@users.noreply.github.com>
2022-09-13 10:16:01 -0400

    FirstLab
    Lab # 1 - My First App

---
+++ AboutMe/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HelloWorldApp() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+    <Text>Student Name: Aurela Bala</Text>
+    <Text>Student ID: 301275255</Text>
+    </View>
+  );}
+
